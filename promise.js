7b74b2d2c5c53dfd519eb2a03e12cc0f7f03e57d
Muhammad Fachri Maulana <mfachri4@gmail.com>
2022-06-12 22:53:43 +0700

    feat: Challenge  07

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (element) => {
+  try {
+    const hasilpromiseTheaterIXX = await promiseTheaterIXX();
+    const hasilpromiseTheaterVGC = await promiseTheaterVGC();
+    const concatHasil = hasilpromiseTheaterIXX.concat(hasilpromiseTheaterVGC);
+
+    return concatHasil.filter((data) => data.hasil === element).length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
